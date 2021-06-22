935e0fcd2d478377a3997855bac84b4f5d11cf82
Danny Lin <danny@kdrag0n.dev>
2021-06-22 01:37:41 -0700

    jetsurvey: Import minimized theme engine from Android 12 Extensions

---
+++ Jetsurvey/app/src/main/java/dev/kdrag0n/android12ext/monet/theme/DynamicColorScheme.kt
@@ -0,0 +1,83 @@
+package dev.kdrag0n.android12ext.monet.theme
+
+import dev.kdrag0n.android12ext.monet.colors.*
+import dev.kdrag0n.android12ext.monet.colors.Oklab.Companion.toOklab
+import dev.kdrag0n.android12ext.monet.colors.Oklch.Companion.toOklch
+
+class DynamicColorScheme(
+    targetColors: ColorScheme,
+    primaryColor: Color,
+    chromaMultiplier: Double = 1.0,
+    private val accurateShades: Boolean = true,
+) : ColorScheme() {
+    private val primaryNeutral = primaryColor.toLinearSrgb().toOklab().toOklch().let { lch ->
+        lch.copy(C = lch.C * chromaMultiplier)
+    }
+    private val primaryAccent = primaryNeutral
+
+    // Main background color. Tinted with the primary color.
+    override val neutral1 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targetColors.neutral1, primaryNeutral)
+    }
+
+    // Secondary background color. Slightly tinted with the primary color.
+    override val neutral2 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targetColors.neutral2, primaryNeutral)
+    }
+
+    // Main accent color. Generally, this is close to the primary color.
+    override val accent1 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targetColors.accent1, primaryAccent)
+    }
+
+    // Secondary accent color. Darker shades of accent1.
+    override val accent2 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        transformSwatch(targetColors.accent2, primaryAccent)
+    }
+
+    // Tertiary accent color. Primary color shifted to the next secondary color via hue offset.
+    override val accent3 by lazy(mode = LazyThreadSafetyMode.NONE) {
+        val primaryA3 = primaryAccent.copy(h = primaryAccent.h + ACCENT3_HUE_SHIFT_DEGREES)
+        transformSwatch(targetColors.accent3, primaryA3)
+    }
+
+    private fun transformSwatch(
+        swatch: Map<Int, Color>,
+        primary: Lch,
+    ): Map<Int, Color> {
+        return swatch.map { (shade, color) ->
+            val target = color as? Lch
+                ?: color.toLinearSrgb().toOklab().toOklch()
+            val newLch = transformColor(target, primary)
+            val newSrgb = newLch.toLinearSrgb().toSrgb()
+
+            shade to newSrgb
+        }.toMap()
+    }
+
+    private fun transformColor(target: Lch, primary: Lch): Color {
+        // Keep target lightness.
+        val L = target.L
+        // Allow colorless gray.
+        val C = primary.C.coerceIn(0.0, target.C)
+        // Use the primary color's hue, since it's the most prominent feature of the theme.
+        val h = primary.h
+
+        val srgb = Oklch(L, C, h).toLinearSrgb()
+        val clipMethod = if (accurateShades) {
+            // Prefer lightness
+            OklabGamut.ClipMethod.PRESERVE_LIGHTNESS
+        } else {
+            // Prefer chroma
+            OklabGamut.ClipMethod.PROJECT_TO_LCUSP
+        }
+
+        return OklabGamut.clip(srgb, clipMethod)
+    }
+
+    companion object {
+        // Hue shift for the tertiary accent color (accent3), in degrees.
+        // 60 degrees = shifting by a secondary color
+        private const val ACCENT3_HUE_SHIFT_DEGREES = 60.0
+    }
+}
---
+++ Jetsurvey/app/src/main/java/dev/kdrag0n/android12ext/monet/theme/MaterialYouTargets.kt
@@ -0,0 +1,64 @@
+package dev.kdrag0n.android12ext.monet.theme
+
+import dev.kdrag0n.android12ext.monet.colors.Color
+import dev.kdrag0n.android12ext.monet.colors.Oklch
+
+/*
+ * Default target colors, conforming to Material You standards.
+ *
+ * Mostly derived from:
+ *   - AOSP defaults: Untinted gray neutral colors and teal accent (+60 deg = ~purple).
+ *   - Pixel defaults: Neutral colors are equivalent to AOSP. Main accent is blue.
+ */
+class MaterialYouTargets(
+    private val chromaFactor: Double = 1.0,
+) : ColorScheme() {
+    companion object {
+        // Lightness from AOSP defaults
+        private val LIGHTNESS_MAP = mapOf(
+            0    to 1.00000,
+            10   to 0.98809,
+            50   to 0.95514,
+            100  to 0.91279,
+            200  to 0.82656,
+            300  to 0.74123,
+            400  to 0.65335,
+            500  to 0.56241,
+            600  to 0.48193,
+            700  to 0.39418,
+            800  to 0.30919,
+            900  to 0.22213,
+            1000 to 0.00000,
+        )
+
+        // Accent chroma from Pixel defaults
+        // We use the most chromatic color as the reference
+        // A-1 chroma = avg(default Pixel Blue shades 100-900)
+        // Excluding very bright variants (10, 50) to avoid light bias
+        // A-1 > A-3 > A-2
+        private const val ACCENT1_CHROMA = 0.13281
+        private const val ACCENT2_CHROMA = ACCENT1_CHROMA / 3
+        private const val ACCENT3_CHROMA = ACCENT2_CHROMA * 2
+
+        // Neutral chroma derived from Google's CAM16 implementation
+        // N-2 > N-1
+        private const val NEUTRAL1_CHROMA = ACCENT1_CHROMA / 12
+        private const val NEUTRAL2_CHROMA = NEUTRAL1_CHROMA * 2
+    }
+
+    override val neutral1 = shadesWithChroma(NEUTRAL1_CHROMA)
+    override val neutral2 = shadesWithChroma(NEUTRAL2_CHROMA)
+
+    override val accent1 = shadesWithChroma(ACCENT1_CHROMA)
+    override val accent2 = shadesWithChroma(ACCENT2_CHROMA)
+    override val accent3 = shadesWithChroma(ACCENT3_CHROMA)
+
+    private fun shadesWithChroma(chroma: Double): Map<Int, Color> {
+        // Adjusted chroma
+        val chromaAdj = chroma * chromaFactor
+
+        return LIGHTNESS_MAP.map {
+            it.key to Oklch(it.value, chromaAdj, 0.0)
+        }.toMap()
+    }
+}

ad658921a620a2e59e72c82c26c331bbe80c6079
Danny Lin <danny@kdrag0n.dev>
2021-06-22 01:37:48 -0700

    jetsurvey: Make UI themes dynamic and animate hue

--- Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/theme/Theme.kt
+++ Jetsurvey/app/src/main/java/com/example/compose/jetsurvey/theme/Theme.kt
@@ -16,41 +16,20 @@
 
 package com.example.compose.jetsurvey.theme
 
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
-
-val LightThemeColors = lightColors(
-    primary = Purple700,
-    primaryVariant = Purple800,
-    onPrimary = Color.White,
-    secondary = Color.White,
-    onSecondary = Color.Black,
-    background = Color.White,
-    onBackground = Color.Black,
-    surface = Color.White,
-    onSurface = Color.Black,
-    error = Red800,
-    onError = Color.White
-)
-
-val DarkThemeColors = darkColors(
-    primary = Purple300,
-    primaryVariant = Purple600,
-    onPrimary = Color.Black,
-    secondary = Color.Black,
-    onSecondary = Color.White,
-    background = Color.Black,
-    onBackground = Color.White,
-    surface = Color.Black,
-    onSurface = Color.White,
-    error = Red300,
-    onError = Color.Black
-)
+import dev.kdrag0n.android12ext.monet.colors.Oklch
+import dev.kdrag0n.android12ext.monet.theme.DynamicColorScheme
+import dev.kdrag0n.android12ext.monet.theme.MaterialYouTargets
+import dev.kdrag0n.android12ext.monet.toMaterialColors
 
 val Colors.snackbarAction: Color
     @Composable
@@ -62,13 +41,50 @@ val Colors.progressIndicatorBackground: Color
 
 @Composable
 fun JetsurveyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+    val infiniteTransition = rememberInfiniteTransition()
+    val hue by infiniteTransition.animateFloat(
+        initialValue = 0.0f,
+        targetValue = 360.0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(10000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart,
+        )
+    )
+
+    val colorScheme = DynamicColorScheme(MaterialYouTargets(), Oklch(1.0, 0.2, hue.toDouble()))
+/*
     val colors = if (darkTheme) {
-        DarkThemeColors
+        darkColors(
+            primary = colorScheme.accent1.shade(300),
+            primaryVariant = colorScheme.accent1.shade(600),
+            onPrimary = colorScheme.neutral1.shade(900),
+            secondary = colorScheme.neutral1.shade(900),
+            onSecondary = colorScheme.neutral1.shade(50),
+            background = colorScheme.neutral1.shade(900),
+            onBackground = colorScheme.neutral1.shade(50),
+            surface = colorScheme.neutral1.shade(900),
+            onSurface = colorScheme.neutral1.shade(50),
+            error = Red300,
+            onError = colorScheme.neutral1.shade(900),
+        )
     } else {
-        LightThemeColors
+        lightColors(
+            primary = colorScheme.accent1.shade(700),
+            primaryVariant = colorScheme.accent1.shade(800),
+            onPrimary = colorScheme.neutral1.shade(50),
+            secondary = colorScheme.neutral1.shade(50),
+            onSecondary = colorScheme.neutral1.shade(900),
+            background = colorScheme.neutral1.shade(50),
+            onBackground = colorScheme.neutral1.shade(900),
+            surface = colorScheme.neutral1.shade(50),
+            onSurface = colorScheme.neutral1.shade(900),
+            error = Red800,
+            onError = colorScheme.neutral1.shade(50),
+        )
     }
+*/
     MaterialTheme(
-        colors = colors,
+        colors = colorScheme.toMaterialColors(!darkTheme),
         typography = Typography,
         shapes = Shapes,
         content = content
